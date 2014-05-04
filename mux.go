601da5f57cc2fa364006dade0bac18e44657f420
Tom Burns <tom.burns@jadedpixel.com>
2014-05-04 02:59:19 +0000

    implement add/remove input/output channels

--- mux.go
+++ mux.go
@@ -1,64 +1,105 @@
 package mux
 
-const {
-	ADD_OUTPUT = iota
-	REMOVE_INPUT = iota
+import (
+	"fmt"
+)
+
+const (
+	ADD_OUTPUT    = iota
+	ADD_INPUT     = iota
+	REMOVE_INPUT  = iota
 	REMOVE_OUTPUT = iota
-	SHUTDOWN = iota
-}
+	SHUTDOWN      = iota
+)
 
 type message struct {
-	type int
+	msgType int
 	channel chan interface{}
 }
- 
-type Mux struct {
-	input chan interface{}
-	outputs []chan interface{}
 
+type input struct {
+	ch       chan interface{}
+	shutdown chan bool
+}
+
+type Mux struct {
+	input   chan interface{}
+	outputs []chan interface{}
+	inputs  []input
 	control chan message
 }
 
 func New() *Mux {
 	mux := &Mux{control: make(chan message, 1), input: make(chan interface{}, 1)}
 	go mux.run()
+	return mux
 }
 
 func (m *Mux) run() {
 	for true {
 		select {
-		case msg := <- input
+		case msg := <-m.input:
 			for _, output := range m.outputs {
 				output <- msg
 			}
-		case ctrl := <- control
-			switch(ctrl.type) {
+		case ctrl := <-m.control:
+			switch ctrl.msgType {
+			case ADD_INPUT:
+				in := input{ch: ctrl.channel, shutdown: make(chan bool, 1)}
+				go func(in input) {
+					for true {
+						select {
+						case msg := <-in.ch:
+							m.input <- msg
+						case <-in.shutdown:
+							return
+						}
+					}
+				}(in)
+				m.inputs = append(m.inputs, in)
+			case REMOVE_INPUT:
+				for idx, in := range m.inputs {
+					if in.ch == ctrl.channel {
+						in.shutdown <- true
+						m.inputs[idx] = m.inputs[len(m.inputs)-1]
+						m.inputs = m.inputs[0 : len(m.inputs)-1]
+						break
+					}
+				}
 			case ADD_OUTPUT:
-				m.outputs := append(m.outputs, ctrl.channel)
+				m.outputs = append(m.outputs, ctrl.channel)
+			case REMOVE_OUTPUT:
+				for i, ch := range m.outputs {
+					if ctrl.channel == ch {
+						m.outputs[i] = m.outputs[len(m.outputs)-1]
+						m.outputs = m.outputs[0 : len(m.outputs)-1]
+						break
+					}
+				}
+			case SHUTDOWN:
+				for _, in := range m.inputs {
+					in.shutdown <- true
+				}
+				return
 			}
-
+		}
 	}
 }
 
-func (m *Mux) send(type int, ch chan interface{}) {
-	msg := message{type:type, channel: ch}
+func (m *Mux) send(msgType int, ch chan interface{}) {
+	msg := message{msgType: msgType, channel: ch}
 	m.control <- msg
 }
 
 func (m *Mux) AddInput(input chan interface{}) {
-	go func(input chan interface{}) {
-		select {
-		case msg := <-input
-			m.input <- msg
-		}
-	}(input)
+	m.send(ADD_INPUT, input)
 }
 
 func (m *Mux) RemoveInput(input chan interface{}) {
-	panic("not yet implemented lol")
+	m.send(REMOVE_INPUT, input)
 }
 
-func (m *Mux) AddOutput(output chan interface{} {
+func (m *Mux) AddOutput(output chan interface{}) {
 	m.send(ADD_OUTPUT, output)
 }
 
@@ -70,3 +111,6 @@ func (m *Mux) Shutdown() {
 	m.send(SHUTDOWN, nil)
 }
 
+func String(in interface{}) string {
+	return fmt.Sprintf("%v", in)
+}
