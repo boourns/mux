59f2e5bd34da8cb29794dadce821b7c6e8a60691
Tom Burns <tom.burns@jadedpixel.com>
2014-05-04 02:59:42 +0000

    add test file to repo

---
+++ mux_test.go
@@ -0,0 +1,92 @@
+package mux
+
+import (
+	"testing"
+)
+
+func TestDoubleSendAndReceive(t *testing.T) {
+	m := New()
+	out1 := make(chan interface{}, 1)
+	out2 := make(chan interface{}, 1)
+
+	in1 := make(chan interface{}, 1)
+	in2 := make(chan interface{}, 1)
+
+	m.AddInput(in1)
+	m.AddInput(in2)
+	m.AddOutput(out1)
+	m.AddOutput(out2)
+
+	in1 <- 1
+	o := <-out1
+	if String(o) != "1" {
+		t.Fatalf("did not receive in1 output back on out1, got %v\n", o)
+	}
+
+	o = <-out2
+	if String(o) != "1" {
+		t.Fatalf("did not receive in1 output back on out2, got %v\n", o)
+	}
+
+	in2 <- 1
+	o = <-out1
+	if String(o) != "1" {
+		t.Fatalf("did not receive in2 output back on out1, got %v\n", o)
+	}
+
+	o = <-out2
+	if String(o) != "1" {
+		t.Fatalf("did not receive in2 output back on out2, got %v\n", o)
+	}
+	m.Shutdown()
+}
+
+func TestRemovingOutput(t *testing.T) {
+	m := New()
+	out1 := make(chan interface{}, 1)
+	out2 := make(chan interface{}, 1)
+	out3 := make(chan interface{}, 1)
+
+	in1 := make(chan interface{}, 1)
+
+	m.AddOutput(out1)
+	m.AddOutput(out2)
+	m.AddOutput(out3)
+	m.RemoveOutput(out2)
+
+	m.AddInput(in1)
+
+	in1 <- 1
+	o := <-out1
+	if String(o) != "1" {
+		t.Fatalf("Did not receive 1 back on in1, got %v\n", o)
+	}
+
+	o = <-out3
+	if String(o) != "1" {
+		t.Fatalf("Did not receive 1 back on in1, got %v\n", o)
+	}
+	m.Shutdown()
+}
+
+func TestRemovingInput(t *testing.T) {
+	m := New()
+	out1 := make(chan interface{}, 1)
+
+	in1 := make(chan interface{}, 1)
+	in2 := make(chan interface{}, 1)
+	in3 := make(chan interface{}, 1)
+
+	m.AddOutput(out1)
+	m.AddInput(in1)
+	m.AddInput(in2)
+	m.AddInput(in3)
+
+	m.RemoveInput(in2)
+	in1 <- 1
+	o := <-out1
+	if String(o) != "1" {
+		t.Fatalf("did not receive expected from input, got %v\n", o)
+	}
+	m.Shutdown()
+}
